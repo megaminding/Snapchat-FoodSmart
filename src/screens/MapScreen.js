5264b51b466afba1fc4d6d9b7ee87841554d97e6
Megan Tieu <meganetieu@gmail.com>
2023-08-07 11:53:55 -0700

    apple icon + array

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import MapView, { Marker } from "react-native-maps";
+import MapView, { Marker, Callout } from 'react-native-maps';
 import {
   StyleSheet,
   View,
@@ -10,23 +10,43 @@ import {
 } from "react-native";
 import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-
 import * as Location from "expo-location";
-
 import Ionicons from "react-native-vector-icons/Ionicons";
-
 export default function MapScreen({ navigation }) {
   const tabBarHeight = useBottomTabBarHeight();
   const insets = useSafeAreaInsets();
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
-
   const [currentRegion, setCurrentRegion] = useState({
     latitude: 34.0211573,
-    longitude:  -118.4503864,
+    longitude:34.0211573,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   });
+  const initialMarkers = [
+    {
+      coordinate: { latitude: 34.0211573, longitude: -118.43184127750077 },
+      title: "Marker 1",
+      description: "This is marker 1",
+      image: require("../../assets/snapchat/personalBitmoji.png")
+    },
+    {
+      coordinate: { latitude:34.03100046677114, longitude:-118.43184127750077 },
+      title: "Brothers Cousins",
+      description: "Taskdfjlkasdjflkajsdlk;fjasd;lkf",
+    },
+  ];
+  const [markers, setMarkers] = useState(initialMarkers);
+  // const addMarker = (coordinate) => {
+  //   setMarkers([...markers, { coordinate }]);
+  // };
+
+  const [addingMarker, setAddingMarker] = useState(false);
+
+  const addMarker = (coordinate) => {
+    setMarkers([...markers, { coordinate }]);
+    setAddingMarker(false); // Reset the addingMarker state
+  };
 
   useEffect(() => {
     (async () => {
@@ -35,38 +55,86 @@ export default function MapScreen({ navigation }) {
         setErrorMsg("Permission to access location was denied");
         return;
       }
-
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
       setCurrentRegion({
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
+        latitude: 34.0211573, //location of snap hq
+        longitude: -118.4503864,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
       });
     })();
   }, []);
-
   let text = "Waiting...";
   text = JSON.stringify(location);
-
   return (
     <View style={[styles.container, { marginBottom: tabBarHeight }]}>
+       {/* <View>
+        <TouchableOpacity
+        style={styles.cameraOptions}
+        source={require("../../assets/map_bar.png")} // idk how to make this show
+        />
+      </View> */}
       <MapView
         style={styles.map}
         region={currentRegion}
         showsUserLocation={true}
         showsMyLocationButton={true}
-      />
+        onPress={(event) => {
+          if (addingMarker) {
+            addMarker(event.nativeEvent.coordinate);
+          }
+        }}
 
+        // onLayout={() => { this.mark.showCallout(); }}
+      >
+    {markers.map((marker, index) => (
+        <Marker
+          key={index}
+          coordinate={marker.coordinate}
+          title={marker.title}
+          description={marker.description}
+          // onLayout={() => marker.callout && marker.callout.showCallout()}
+        >
+          {/* <Callout>
+            <View>
+              <Text>{marker.title}</Text>
+              <Text>{marker.description}</Text>
+            </View>
+          </Callout> */}
+          <Image
+            source={require("../../assets/apple.png")}
+            style={{ width: 48, height: 48 }}
+            resizeMode="contain"
+          />
+           <Image
+            source={marker.image}
+            style={{ width: 48, height: 48 }}
+            resizeMode="contain"
+          />
+        </Marker>
+      ))}
+      </MapView>
       <View style={[styles.mapFooter]}>
         <View style={styles.locationContainer}>
           <TouchableOpacity
             style={[styles.userLocation, styles.shadow]}
             onPress={() => {
-              console.log("Go to user location!");
+              console.log("Go to user location! - top");
               const { latitude, longitude } = location.coords;
               setCurrentRegion({ ...currentRegion, latitude, longitude });
+            }}
+          >
+            <Ionicons name="ios-navigate" size={15} color="black" />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.locationContainer}>
+          <TouchableOpacity
+            style={[styles.userLocation, styles.shadow]}
+            onPress={() => {
+              console.log("bottom");
+              setAddingMarker(!addingMarker)
+             
             }}
           >
             <Ionicons name="ios-navigate" size={15} color="black" />
@@ -105,13 +173,31 @@ export default function MapScreen({ navigation }) {
     </View>
   );
 }
-
 const styles = StyleSheet.create({
+  cameraOptions: {
+    backgroundColor: 'white',
+    position: "absolute",
+    right: 12,
+    paddingTop: 8,
+    height: 250,
+    width: 40,
+    padding: 5,
+  },
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  mapyFooter: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    position: "absolute",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingBottom: 20,
+    bottom: 0,
   },
   mapFooter: {
     width: "100%",
